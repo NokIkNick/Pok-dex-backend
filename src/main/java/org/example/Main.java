6a5068e6bd74086926aa850d94d6a7d151878f77
Nicklas Winther <Nicklas2002@hotmail.com>
2024-06-01 23:34:53 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,8 @@
 package org.example;
 
-import io.javalin.Javalin;
 import org.example.config.ApplicationConfig;
 import org.example.config.Routes;
-import org.example.daos.TestMemoryDao;
-import org.example.dtos.TestDTO;
 
-import java.util.List;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
